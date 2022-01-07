53c7ce0abfb4cc60360eec37822092ef9e90a53f
ExPlo1t-php <elabbas.younes.solicode@gmail.com>
2022-01-07 16:56:45 +0100

    html and CSS update

---
+++ script.js
@@ -0,0 +1,31 @@
+<div class="moreInfo row w-100">
+<h3 class="h3">More Informations</h3>
+<div class="col w-50">
+        <div class="row">
+            <span class="col w-25">UV</span>
+            <span class="col val">4</span>
+        </div>
+        <div class="row">
+            <span class="col w-25">Pressure</span>
+            <span class="col val">1025 Mbar</span>
+        </div>
+        <div class="row">
+            <span class="col w-25">Cloud</span>
+            <span class="col val">75</span>
+        </div>
+</div>
+<div class="col w-50">
+        <div class="row">
+            <span class="col">Time Zone</span>
+            <span class="col">Africa/Casablanca</span>
+        </div>
+        <div class="row">
+            <span class="col">LocalTime</span>
+            <span class="col val">
+                <span class="col">2022-01-07</span>
+                <span class="col">11:09</span>
+            </span>
+        </div>
+</div>
+
+</div>
